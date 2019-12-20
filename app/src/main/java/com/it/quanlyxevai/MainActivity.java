ec720b38ecd46828ca12da250d19bbd4b1ed3df1
TruyenLam <lamvantruyen@gmail.com>
2019-12-20 21:31:03 +0700

    hoan thanh login

--- app/src/main/java/com/it/quanlyxevai/MainActivity.java
+++ app/src/main/java/com/it/quanlyxevai/MainActivity.java
@@ -37,58 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(MainActivity.this,keytoken,Toast.LENGTH_LONG).show();
     }
-    public void getHttpResponse() throws IOException {
-        //tạo url có parameter
-        //String url = "https://local.thttextile.com.vn/thtapigate/api/QLSX/XeVai/Get_YeuCauXe";
 
-        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://local.thttextile.com.vn/thtapigate/api/QLSX/XeVai/Get_YeuCauXe").newBuilder();
-        urlBuilder.addQueryParameter("yeucauxe", "");
-        String url = urlBuilder.build().toString();
-
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(url)
-                .header("Accept", "application/json")
-                .header("Content-Type", "application/json")
-                .header("Authorization", "Bearer " + keytoken)
-                .build();
-
-//        Response response = client.newCall(request).execute();
-//        Log.e(TAG, response.body().string());
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                String mMessage = e.getMessage().toString();
-                Log.w("failure Response", mMessage);
-                //call.cancel();
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                String mMessage = response.body().string();
-                Log.e(TAG, mMessage); //đã thành công: thấy log ghi json string là các yêu cầu về xe
-                //Dữ liệu cho listview dạng danh sách hoặc mảng --> cần chuyển danh sách các đối tượng ra mảng
-                try {
-                    // cast ra JSONArray
-                    JSONArray jArray = new JSONArray(mMessage);
-                    // implement for loop for getting users list data
-                    for (int i = 0; i < jArray.length(); i++) {
-                        // create a JSONObject for fetching single user data
-                        JSONObject yeuCauXeDetail = jArray.getJSONObject(i);
-                        //dua vao cai arrayobject
-                        String id = yeuCauXeDetail.getString("Id");
-                        String ngayYeuCau = yeuCauXeDetail.getString("NgayYeuCau");
-                        Log.e(TAG, id + " " + ngayYeuCau );
-
-
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
 }
 
 
